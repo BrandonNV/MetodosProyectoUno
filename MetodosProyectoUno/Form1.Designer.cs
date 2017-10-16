471263052c6a2b56374ff7bdc69bb112c6f83226
BrandonNV <bnavarrog.12@gmail.com>
2017-10-15 20:48:53 -0500

    Initial commit

---
+++ MetodosProyectoUno/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace MetodosProyectoUno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.g = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.o = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.xi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ri = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.semilla = new System.Windows.Forms.TextBox();
+            this.multiplicador = new System.Windows.Forms.TextBox();
+            this.incremento = new System.Windows.Forms.TextBox();
+            this.modulo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.veces = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.GhostWhite;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.g,
+            this.o,
+            this.xi,
+            this.ri});
+            this.dataGridView1.Location = new System.Drawing.Point(195, 119);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(610, 354);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // g
+            // 
+            this.g.HeaderText = "Generador";
+            this.g.Name = "g";
+            // 
+            // o
+            // 
+            this.o.HeaderText = "Operación";
+            this.o.Name = "o";
+            // 
+            // xi
+            // 
+            this.xi.HeaderText = "Xi";
+            this.xi.Name = "xi";
+            // 
+            // ri
+            // 
+            this.ri.HeaderText = "Ri";
+            this.ri.Name = "ri";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(461, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // semilla
+            // 
+            this.semilla.Location = new System.Drawing.Point(172, 30);
+            this.semilla.Name = "semilla";
+            this.semilla.Size = new System.Drawing.Size(100, 22);
+            this.semilla.TabIndex = 3;
+            // 
+            // multiplicador
+            // 
+            this.multiplicador.Location = new System.Drawing.Point(301, 30);
+            this.multiplicador.Name = "multiplicador";
+            this.multiplicador.Size = new System.Drawing.Size(100, 22);
+            this.multiplicador.TabIndex = 4;
+            // 
+            // incremento
+            // 
+            this.incremento.Location = new System.Drawing.Point(442, 30);
+            this.incremento.Name = "incremento";
+            this.incremento.Size = new System.Drawing.Size(100, 22);
+            this.incremento.TabIndex = 5;
+            // 
+            // modulo
+            // 
+            this.modulo.Location = new System.Drawing.Point(573, 30);
+            this.modulo.Name = "modulo";
+            this.modulo.Size = new System.Drawing.Size(100, 22);
+            this.modulo.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(192, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Semilla";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(310, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Multiplicador";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(450, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Incremento";
+            // 
+            // veces
+            // 
+            this.veces.Location = new System.Drawing.Point(706, 30);
+            this.veces.Name = "veces";
+            this.veces.Size = new System.Drawing.Size(100, 22);
+            this.veces.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(595, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 18);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Módulo";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(730, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Veces";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(1001, 498);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.veces);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.modulo);
+            this.Controls.Add(this.incremento);
+            this.Controls.Add(this.multiplicador);
+            this.Controls.Add(this.semilla);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn g;
+        private System.Windows.Forms.DataGridViewTextBoxColumn o;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ri;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox semilla;
+        private System.Windows.Forms.TextBox multiplicador;
+        private System.Windows.Forms.TextBox incremento;
+        private System.Windows.Forms.TextBox modulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox veces;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MetodosProyectoUno/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodosProyectoUno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(semilla.Text) || semilla.Text.Any(Char.IsLetter) ||
+                string.IsNullOrWhiteSpace(multiplicador.Text) || multiplicador.Text.Any(Char.IsLetter) || 
+                string.IsNullOrWhiteSpace(incremento.Text) || incremento.Text.Any(Char.IsLetter) ||
+                string.IsNullOrWhiteSpace(modulo.Text) || modulo.Text.Any(Char.IsLetter) ||
+                string.IsNullOrWhiteSpace(veces.Text) || veces.Text.Any(Char.IsLetter)
+                )
+            {
+                MessageBox.Show("Favor de ingresar únicamente valores númericos");
+            }
+            else
+            {
+                dataGridView1.Rows.Clear();
+                string[] rows = new string[300];
+                int s = int.Parse(semilla.Text);
+                int mu = int.Parse(multiplicador.Text);
+                int i = int.Parse(incremento.Text);
+                int mo = int.Parse(modulo.Text);
+                int n = int.Parse(veces.Text);
+                //int s = 4;
+                //int  mu = 5;
+                //int i = 7;
+                //int mo = 8;
+                double convert = 0;
+                string temp;
+
+
+                for (int a = 0; a < n; a++)
+                {
+
+                    rows[0] = s.ToString();
+                    convert = Convert.ToDouble(mu * s + i);
+                    temp = (convert / 8).ToString() + ".";
+                    string[] operacion = temp.Split('.');
+                    if (operacion[1] != "") operacion[1] = " + ." + operacion[1];
+                    rows[1] = operacion[0] + "" + operacion[1];
+                    rows[2] = (convert % 8).ToString();
+                    rows[3] = ((convert % 8) / 8).ToString();
+                    dataGridView1.Rows.Add(rows);
+                    s = (mu * s + i) % mo;
+
+                }
+
+            }
+
+        }
+    }
+}
